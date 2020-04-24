4cf4a6121e919045359e94305a3619262d8b7252
Saba Rehman <sabarehman1998@gmail.com>
2020-04-24 17:41:00 +0500

    added map API

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: SplashScreen(),
+      home: Ops(),
       routes: {
         '/login_student' : (context) => LoginStudent(),
         '/login_ems_mfr' : (context) => LoginEms('mfr'),
