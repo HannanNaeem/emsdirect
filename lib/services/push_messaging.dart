10b00812f49c37820f9149cd5da82d2dddea71be
Hannan Naeem <hannan.2455@gmail.com>
2020-05-02 07:25:09 +0500

    integrated cloud messaging

--- lib/services/push_messaging.dart
+++ lib/services/push_messaging.dart
@@ -1,1 +1,31 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
+
+class CloudMessagingService {
+
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+
+  //get token
+  void getToken(){
+    _firebaseMessaging.getToken().then((deviceToken){
+      print("Device Token: $deviceToken");
+    });
+  }
+
+  void configureFirebaseListeners() {
+    _firebaseMessaging.configure(
+      onMessage: (Map<String,dynamic> message) async {
+        print('onMessage: $message');
+      },
+      onLaunch: (Map<String,dynamic> message) async {
+        print('onMessage: $message');
+      },
+      onResume: (Map<String,dynamic> message) async {
+        print('onMessage: $message');
+      },
+      onBackgroundMessage: (Map<String,dynamic> message) async {
+        print('onMessage: $message');
+      },
+    );
+  }
+
+}
