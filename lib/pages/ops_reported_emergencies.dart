8de240a68e08390885c00d81b80ba10b7a612581
Hannan Naeem <hannan.2455@gmail.com>
2020-05-05 18:02:43 +0500

    OnDemand fetch Working

--- lib/pages/ops_reported_emergencies.dart
+++ lib/pages/ops_reported_emergencies.dart
@@ -5,15 +5,19 @@ import 'package:ems_direct/shared/reported_emergency_tile.dart';
 import "package:flutter/material.dart";
 import 'package:intl/intl.dart';
 
+GlobalKey _globalKey = GlobalKey();
 
 class ReportedEmergenciesOps extends StatefulWidget {
+  Key key = _globalKey;
   @override
   _ReportedEmergenciesOpsState createState() => _ReportedEmergenciesOpsState();
 }
 
 class _ReportedEmergenciesOpsState extends State<ReportedEmergenciesOps> {
 
-  int _maxDocs = 20;
+  int _maxDocs = 1; //todo reset
+  int _step = 1; //add additional docs
+  bool _firstLoad = true;
   var collectionRef = Firestore.instance.collection('ReportedEmergencies');
   var docs;
 
@@ -41,9 +45,24 @@ class _ReportedEmergenciesOpsState extends State<ReportedEmergenciesOps> {
 
   Future _getInitialDocs() async { //used to perform the initial fetch
     try{
-      QuerySnapshot snapshot = await collectionRef.orderBy('date', descending: true).limit(20).getDocuments();
+      QuerySnapshot snapshot = await collectionRef.orderBy('date', descending: true).limit(_maxDocs).getDocuments();
       if(snapshot!= null)
         _reportedEmergencies = _getEmergencyList(snapshot);
+      return snapshot;
+    }
+    catch(e) {
+      print(e);
+      return null;
+    }
+  }
+
+  Future _getAdditionalDocs() async { //used to perform the initial fetch
+    try{
+      QuerySnapshot snapshot = await collectionRef.orderBy('date', descending: true).limit(_maxDocs).getDocuments();
+      if(snapshot!= null)
+      setState(() {
+        _reportedEmergencies = _getEmergencyList(snapshot);
+      });
       return snapshot;
     }
     catch(e) {
@@ -79,9 +98,6 @@ class _ReportedEmergenciesOpsState extends State<ReportedEmergenciesOps> {
     });
     return output;
   }
-
-
-
 
   @override
   Widget build(BuildContext context) {
@@ -120,8 +136,10 @@ class _ReportedEmergenciesOpsState extends State<ReportedEmergenciesOps> {
 
                     Expanded(
                       child: ListView.builder(
-                        itemCount: _reportedEmergencies.length,
+                        itemCount: _reportedEmergencies.length + 1,
                         itemBuilder: (context, index){
+                          
+                          if(index < _reportedEmergencies.length)
                           return Padding(
                             padding: EdgeInsets.fromLTRB(20, 10, 20, 0),
                             child: ReportedEmergencyTile(
@@ -135,11 +153,24 @@ class _ReportedEmergenciesOpsState extends State<ReportedEmergenciesOps> {
                               patientIsHostelite: _reportedEmergencies[index].patientIsHostelite,
                               emergencyType: _reportedEmergencies[index].emergencyType,
                               emergencyLocation: _reportedEmergencies[index].emergencyLocation == "" ? "No location" :  _reportedEmergencies[index].emergencyLocation,
-                              transportUsed: _reportedEmergencies[index].transportUsed == "Yes" ? "  |  Transport Used" : "",
+                              transportUsed: _reportedEmergencies[index].transportUsed == "Yes" ? "Transport Used" : "",
                               emergencyDetails: _reportedEmergencies[index].emergencyDetails == "" ? "No details" : _reportedEmergencies[index].emergencyDetails,
                               bagUsed: _reportedEmergencies[index].bagUsed,
                               equipmentUsed: _reportedEmergencies[index].equipmentUsed == null ? "None" : _translateMap(_reportedEmergencies[index].equipmentUsed),
                             ),
+                          );
+                          else
+                          return(
+                            RaisedButton(
+                              child: Text("Load more"),
+                              onPressed: () async {
+                                setState(() {
+                                  _firstLoad = false;
+                                  _maxDocs++;
+                                  _getAdditionalDocs();
+                                });
+                              },
+                            )
                           );
                         }
                       ),
@@ -158,12 +189,65 @@ class _ReportedEmergenciesOpsState extends State<ReportedEmergenciesOps> {
                       color: Colors.grey[600],
                       fontSize: 15,
                     ),
-
                   ),
                 );
               }
           } else { //!display loading as fetch isnt compelete
-            return Loading();
+            if(_firstLoad){
+              return Loading();
+            }
+            else{ //! Display the old state if this isnt the first load
+                return Container(
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: _reportedEmergencies.length + 1,
+                        itemBuilder: (context, index){
+                          
+                          if(index < _reportedEmergencies.length)
+                          return Padding(
+                            padding: EdgeInsets.fromLTRB(20, 10, 20, 0),
+                            child: ReportedEmergencyTile(
+                              patientRollNo: _reportedEmergencies[index].patientRollNo,
+                              patientGender: _reportedEmergencies[index].patientGender,
+                              emergencyDate: DateFormat.MMMd().format(_reportedEmergencies[index].emergencyDate) + ", " + DateFormat.E().format(_reportedEmergencies[index].emergencyDate) + "\n"+ DateFormat.Hm().format(_reportedEmergencies[index].emergencyDate),
+                              primaryMfrRollNo: _reportedEmergencies[index].primaryMfrRollNo,
+                              primaryMfrName: _reportedEmergencies[index].primaryMfrName,
+                              additionalMfrs: _reportedEmergencies[index].additionalMfrs == "" ? "None" : _reportedEmergencies[index].additionalMfrs,
+                              severity: _reportedEmergencies[index].severity,
+                              patientIsHostelite: _reportedEmergencies[index].patientIsHostelite,
+                              emergencyType: _reportedEmergencies[index].emergencyType,
+                              emergencyLocation: _reportedEmergencies[index].emergencyLocation == "" ? "No location" :  _reportedEmergencies[index].emergencyLocation,
+                              transportUsed: _reportedEmergencies[index].transportUsed == "Yes" ? "Transport Used" : "",
+                              emergencyDetails: _reportedEmergencies[index].emergencyDetails == "" ? "No details" : _reportedEmergencies[index].emergencyDetails,
+                              bagUsed: _reportedEmergencies[index].bagUsed,
+                              equipmentUsed: _reportedEmergencies[index].equipmentUsed == null ? "None" : _translateMap(_reportedEmergencies[index].equipmentUsed),
+                            ),
+                          );
+                          else
+                          return(
+                            RaisedButton(
+                              child: Text("Load more"),
+                              onPressed: () async {
+                                setState(() {
+                                  _firstLoad = false;
+                                  _maxDocs++;
+                                  _getAdditionalDocs();
+                                });
+                              },
+                            )
+                          );
+                        }
+                      ),
+                    ),
+
+                  ],)
+                );
+            }
           }
         }
       ),
